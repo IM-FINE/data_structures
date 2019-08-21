9172f9b28c7a5daf127597cd99b3a5243e4a869c
jyh121920709 <jyh121920709@163.com>
2019-08-21 22:38:46 +0800

    20190821

--- liner_list/2_linked_storage.py
+++ liner_list/2_linked_storage.py
@@ -3,41 +3,61 @@
 
 class LinkedStorage(object):
     class Node(object):
-        def __init__(self, element, nex):
-            self.element = element
-            self.nex = nex
+        def __init__(self, data, next):
+            self.data = data
+            self.next = next
 
     def __init__(self):
 
         self.size = 0
         # 头结点
         self.head_node = self.Node(None, None)
-        # 头指针
-        self.head = self.head_node
+        # 头指针(头节点的内存地址)
+        # self.head_pointer = self.head_node
 
     def show_list(self):
-        print(self.head_node.nex.element, end=' ')
-        print(self.head_node.nex.nex.element, end=' ')
+        for i in range(1, self.size + 1):
+            if i == self.size:
+                print(self.get_elem(i))
+            else:
+                print(self.get_elem(i), end=' ')
 
     def get_elem(self, i):
-        pass
+        p = self.head_node.next
+        j = 1
+        while p and (j < i):
+            p = p.next
+            j += 1
+        if (p is None) or (j > i):
+            return f'{i}位置节点不存在'
+        return p.data
 
     def list_insert(self, i, e):
 
         j = 1
         p = self.head_node
-        while (p.nex and (j < i)):
-            p = p.nex
+        while (p.next and (j < i)):
+            p = p.next
             j += 1
-        if ((p == None) or (j > i)):
+        if (p is None) or (j > i):
             raise Exception('...')
 
-        s = self.Node(e, p.nex)
-        p.nex = s
+        s = self.Node(e, p.next)
+        p.next = s
         self.size += 1
 
     def list_delete(self, i):
-        pass
+        p = self.head_node
+        j = 1
+        while (p.next) and (j < i):
+            p = p.next
+            j += 1
+        if (not p.next) or (j > i):
+            return f'{i}位置节点不存在'
+        q = p.next
+        p.next = q.next
+        self.size -= 1
+        return q.data
 
     def get_length(self):
         return self.size
@@ -47,5 +67,8 @@ if __name__ == '__main__':
     l = LinkedStorage()
     l.list_insert(1, 'a')
     l.list_insert(2, 'b')
+    l.list_insert(3, 'c')
     l.show_list()
-    print(l.get_length())
+    print(l.get_elem(3))
+    print(l.list_delete(2))
+    l.show_list()
--- liner_list/test.py
+++ liner_list/test.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+a='aaa'
+
+print(id(a))
+print(id('aaa'))
