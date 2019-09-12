e5baa52c21fbf2b26c479be630571ab3f69d61cb
jyh121920709 <jyh121920709@163.com>
2019-09-12 16:29:42 +0800

    20190912

---
+++ stack/1_arithmetic_operations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from algorithms import stack
+
+
+class ArithOper(object):
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    o = ArithOper()
+    o.main()
